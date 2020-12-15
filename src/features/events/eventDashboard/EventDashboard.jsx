2c5ae0c2d5766b19bc4c5966012afec53e4f93c8
Habiburrahman Dehzad <rhabiblv@gmail.com>
2020-12-15 17:25:53 +0430

    End of section 4

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,22 +1,44 @@
-import React, { useState } from 'react';
-import { Grid } from 'semantic-ui-react';
-import { sampleData } from '../../../app/api/sampleData';
-import EventForm from '../eventForm/EventForm';
-import EventList from './EventList';
+import React, { useState } from "react";
+import { Grid } from "semantic-ui-react";
+import { sampleData } from "../../../app/api/sampleData";
+import EventForm from "../eventForm/EventForm";
+import EventList from "./EventList";
 
-const EventDashboard = ({formOpen, setFormOpen}) => {
-    const [events, setEvents] = useState(sampleData);
+const EventDashboard = ({ formOpen, setFormOpen, selectEvent, selectedEvent }) => {
+  const [events, setEvents] = useState(sampleData);
 
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <EventList events={events} />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                {formOpen && <EventForm setFormOpen={setFormOpen} />}
-            </Grid.Column>
-        </Grid>
-    )
-}
+  const handleCreateEvent = (event) => {
+    setEvents([...events, event]);
+  };
 
-export default EventDashboard
+  const handleUpdateEvent = (updatedEvent) => {
+    setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
+    selectEvent(null);
+  }
+
+  const handleDeleteEvent = (eventId) => {
+    setEvents(events.filter(evt => evt.id !== eventId));
+  }
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <EventList events={events} selectEvent={selectEvent} deleteEvent={handleDeleteEvent} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+            selectedEvent={selectedEvent}
+            updateEvent={handleUpdateEvent}
+            key={selectedEvent ? selectedEvent.id : null}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default EventDashboard;
