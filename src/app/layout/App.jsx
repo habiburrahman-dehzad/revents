ab5ddfee61bac036c10e1f61d4bb0489137c57dc
Habiburrahman Dehzad <rhabiblv@gmail.com>
2020-12-15 20:10:35 +0430

    End of section 5

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,33 +1,30 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
+import { Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar";
+import HomePage from "../../features/home/HomePage";
+import EventDetailedPage from "../../features/events/eventDetailed/EventDetailedPage";
+import EventForm from "../../features/events/eventForm/EventForm";
 
 function App() {
-  const [formOpen, setFormOpen] = useState(false);
-  const [selectedEvent, setSelectedEvent] = useState(null);
-
-  const handleSelectEvent = (event) => {
-    setSelectedEvent(event);
-    setFormOpen(true);
-  };
-
-  const handleCreateFormOpen = () => {
-    setSelectedEvent(null);
-    setFormOpen(true);
-  };
 
   return (
     <Fragment>
-      <NavBar setFormOpen={handleCreateFormOpen} />
-      <Container className='main'>
-        <EventDashboard
-          formOpen={formOpen}
-          setFormOpen={setFormOpen}
-          selectEvent={handleSelectEvent}
-          selectedEvent={selectedEvent}
-        />
-      </Container>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path='/(.+)'
+        render={() => (
+          <Fragment>
+            <NavBar />
+            <Container className='main'>
+              <Route exact path='/events' component={EventDashboard} />
+              <Route path='/events/:id' component={EventDetailedPage} />
+              <Route path={['/createEvent', '/manage/:id']} component={EventForm} />
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'semantic-ui-css/semantic.min.css';
-import './app/layout/styles.css';
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 
 const render = () => {
-  ReactDOM.render(<App />, rootElement);
-}
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement
+  );
+};
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App', () => {
+  module.hot.accept("./app/layout/App", () => {
     setTimeout(render);
   });
 }
